b1d203b215db2dc44d01569aa04ae8c4ea3a62aa
Prajwal-PL <prajwalpl096@gmail.com>
2025-03-03 18:45:15 +0530

    init + adding shadcn with tailwind css

---
+++ apps/web/lib/page-metadata.ts
@@ -0,0 +1,34 @@
+export const metadata = {
+  dashboard: {
+    title: "Dashboard",
+    description: "Analyze your project information",
+  },
+  workspace: {
+    title: "Workspace",
+    description: "Manage your project workspace",
+  },
+  community: {
+    title: "Community",
+    description: "Explore community projects",
+  },
+  settings: {
+    title: "Settings",
+    description: "Manage your settings and preferences",
+  },
+  history: {
+    title: "History",
+    description: "View your project history",
+  },
+  billing: {
+    title: "Billing",
+    description: "Manage your billing information",
+  },
+  favorites: {
+    title: "Favorites",
+    description: "View your favorite projects",
+  },
+  yourProjects: {
+    title: "Your Projects",
+    description: "Explore all of your private and public projects",
+  },
+};
