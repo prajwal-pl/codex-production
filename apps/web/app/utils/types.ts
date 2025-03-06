b9bca1d4476960f5fd9afd83855a32cff02f38eb
Prajwal-PL <prajwalpl096@gmail.com>
2025-03-06 22:28:39 +0530

    Adding types and constants

---
+++ apps/web/app/utils/types.ts
@@ -0,0 +1,13 @@
+export interface Language {
+  id: string;
+  label: string;
+  logoPath: string;
+  monacoLanguage: string;
+  defaultCode: string;
+  pistonRuntime: LanguageRuntime;
+}
+
+export interface LanguageRuntime {
+  language: string;
+  version: string;
+}
