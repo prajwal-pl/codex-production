55f4790d151d075fdce02ccbbea86941fcf6e9cd
Prajwal-PL <prajwalpl096@gmail.com>
2025-03-17 22:44:29 +0530

    Completing structural components for landing page

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,10 @@
-import Features from "components/features-1";
+import CallToAction from "components/landing-page/call-to-action";
+import ContentSection from "components/landing-page/content-1";
+import Features from "components/landing-page/features-1";
+import FooterSection from "components/landing-page/footer";
 import HeroSection from "components/landing-page/hero-section";
+import StatsSection from "components/landing-page/stats";
+import Testimonials from "components/landing-page/testimonials";
 import React from "react";
 
 const Home = () => {
@@ -7,6 +12,11 @@ const Home = () => {
     <div>
       <HeroSection />
       <Features />
+      <ContentSection />
+      <StatsSection />
+      <Testimonials />
+      <CallToAction />
+      <FooterSection />
     </div>
   );
 };
