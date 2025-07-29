25bae365401c2ec87eb9689eb5643c0e827d3609
Prajwal-PL <prajwalpl096@gmail.com>
2025-07-29 21:29:08 +0530

    re adding common middleware package, removing tsconfig.json for simplicity

--- apps/worker-be/src/routes/project.route.ts
+++ apps/worker-be/src/routes/project.route.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { createProjectHandler } from "../controllers/project.controller";
-import { authMiddleware } from "@repo/common/src";
+import { authMiddleware } from "@repo/common";
 
 const router = express.Router();
 
